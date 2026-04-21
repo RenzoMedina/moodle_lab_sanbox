31affd05f0593c6e64daa90cba6743a5836bbfcd
RenzoMedina <medinast30@gmail.com>
2026-04-20 22:39:41 -0400

    Add font,footer and drawers

--- plugins/theme/moodle_lab/lib.php
+++ plugins/theme/moodle_lab/lib.php
@@ -22,7 +22,3 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-
-function theme_moodle_lab_get_main_scss_content($theme) {
-    return '';
-}
--- plugins/theme/moodle_lab/tailwind.config.js
+++ plugins/theme/moodle_lab/tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
     preflight: false,
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans : ['Saira'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+    },
   },
   plugins: [],
 }
